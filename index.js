a1a2d57d661e254526ca9c3f459d4d872b220255
tocomi <tocomi@example.com>
2018-11-17 03:50:01 +0900

    make random message

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const lineClient = new line.Client(lineConfig)
 async function replyMessage(replyToken, userName, message) {
   let reply = undefined
 
+  reply = messageRandom()
+
   if (containsOsusume(message)) {
     reply = userName + 'さんへのオススメは、\n「' + selectSongRandom() + '」\nです！'
   }
@@ -20,6 +22,26 @@ async function replyMessage(replyToken, userName, message) {
   if (reply) {
     console.log(`replyMessage: ${reply}`)
     await lineClient.replyMessage(replyToken, { type: 'text', text: reply })
+  }
+}
+
+const messageList = [
+  '京都発4ピース和風ロックバンド。 2012年4月結成。\n結成以来、京都を中心に関西の様々なライブハウスやイベントで精力的にライブを行う。',
+  '2012年9月、レコ発であり初の自主企画ライブを京都ソクラテスにて開催。\n多くの応援のもと70人を動員し、会場を満員にする。第一弾音源「暁」を発売。',
+  '2013年3月、結成からわずか1年弱ながら、京都のバンド大会「ライブキッズvol.23」の一般部門ファイナリスト5組に選出される。\nまた、「十代白書」「トリガーフェス」「SOUND SHOCK」「Ro69jack」など大型フェスの上位審査へと駒を進める。',
+  '2013年5月、JEUGIA主催「大学対抗MusicLiveChampionship」にてグランプリを受賞。',
+  '2013年5月には2度目となるレコ発の自主企画ライブを京都MOJOにて開催。\n次世代の関西バンドシーンを担う若手有力バンドを集め、第2弾音源「錦」を発売。約100人を動員する。',
+  '2013年9月、関西圏「学園祭ツアー」を開始。同志社大、京都府立大、大阪府立大、京都教育大、京都造形芸術大の計5大学の学園祭にてパフォーマンスを行う。',
+  '2013年10月、横浜スタジアムにて「新横浜パフォーマンス2013」に出演。湘南乃風の『若旦那』との共演を果たす。\nまた、花火大会や寺社ライブなど様々な野外イベントでライブを行う。',
+  '2014年3月には3度目にして最大規模の自主企画ライブ「都フェス」を京都FANJにて開催。\n関西若手インディーズシーンの先頭に立つバンドを集め、約100人を動員する大盛況のイベントとなる。',
+  '2014年3月、「ライブキッズvol.24」へ2年連続の決勝進出を果たす。',
+  '和風を基調としたメロディと文学的でストーリー性のある詞、力強い女性ボーカルと激しいギターサウンドが絡み合う「和風ロック」を武器に精力的に活動中。'
+]
+function messageRandom() {
+  try {
+    return messageList[Math.floor(Math.random() * 30)]
+  } catch(e) {
+    return undefined
   }
 }
 
